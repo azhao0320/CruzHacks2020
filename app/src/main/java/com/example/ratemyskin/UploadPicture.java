cb0ee0fc4fb25b652d6a6964a7ba20bc3583f4f1
Huanlei Wu <huanleiwu@gmail.com>
2020-01-19 05:07:07 -0800

    implemented taking picture or uploading from gallery + firebase

---
+++ app/src/main/java/com/example/ratemyskin/UploadPicture.java
@@ -0,0 +1,26 @@
+package com.example.ratemyskin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class UploadPicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_picture);
+
+        ImageView photoImageView = findViewById(R.id.comparePicture);
+//        Intent intent = getIntent();
+//        ImageView photo = intent.getImageViewExtra(MainActivity.EXTRA_PHOTO);
+
+    }
+}
