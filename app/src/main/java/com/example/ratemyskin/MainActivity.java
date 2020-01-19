c394b7ae1f4c2982c30dbe3a2170f55a42d035f6
Huanlei Wu <huanleiwu@gmail.com>
2020-01-18 19:24:41 -0800

    init commit

--- app/src/main/java/com/example/ratemyskin/MainActivity.java
+++ app/src/main/java/com/example/ratemyskin/MainActivity.java
@@ -1,14 +1,89 @@
+// camera tutorial: https://www.youtube.com/watch?v=32RG4lR0PMQ
+// firebase tutorial:
+
 package com.example.ratemyskin;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import android.util.Log;
+import android.view.View;
 
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 public class MainActivity extends AppCompatActivity {
+
+    String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Hackerman", "I'm in");
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+
+        FloatingActionButton fab = findViewById(R.id.cameraBtn);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("camera", "camera button has been clicked");
+                dispatchCameraAction();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                // Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                Log.d("PIC", "Picture taken.");
+            }
+        }
+    }
+
+    private void dispatchCameraAction() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            File photoFile = createPhotoFile();
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.example.ratemyskin.fileprovider", photoFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePicture,1);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String photoName = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(photoName, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("mylog", "Excep : " + e.toString());
+        }
+        return image;
     }
 }
